8906e2a871747c85a9492599e51a0b2dec1cafe6
Marios <38200565+MariosMarinos@users.noreply.github.com>
2019-12-30 15:15:58 +0200

    update

--- Final/letter_classifier.py
+++ Final/letter_classifier.py
@@ -10,17 +10,54 @@ Therefore, when working with this classifier, it's best to collect your own data
 
 #Line follower code here
 
-def classify():
+from ev3dev.auto import *
+import pickle
+
+
+def run():
+        # Connect motors
+    left_motor = LargeMotor(OUTPUT_B)
+    assert left_motor.connected
+    right_motor = LargeMotor(OUTPUT_C)
+    assert right_motor.connected
+
+    # Connect touch sensor and color sensors
+    ts = TouchSensor()
+    assert ts.connected
+    col_left = ColorSensor('in1')
+    assert col_left.connected
+    col_right = ColorSensor('in4')
+    assert col_right.connected
+
+    # Change color sensor mode
+    col_left.mode = 'COL-REFLECT'
+    col_right.mode = 'COL-REFLECT'
+    left = []
+    right = []
+    thresh = 55
+    while not ts.value():
+        # Add sensor values to respective list
+        left.append(col_left.value())
+        right.append(col_right.value())
+
+        if left[-1] > thresh and right[-1] > thresh:
+            right_motor.run_forever(speed_sp=90)
+            left_motor.run_forever(speed_sp=90)
+        if left[-1] < thresh:
+            left_motor.run_forever(speed_sp=0)
+            right_motor.run_forever(speed_sp=100)
+        if right[-1] < thresh:
+            right_motor.run_forever(speed_sp=0)
+            left_motor.run_forever(speed_sp=100)
     
-    left_sensor = left_sensor / 557
-    mid_sensor = mid_sensor / 557
-    right_sensor = right_sensor / 557
-    left_motor_count = left_motor_count / 557
-    right_motor_count = right_motor_count / 557
+    col_left = col_left / 557
+    col_right = col_right / 557
+    left_motor= left_motor / 557
+    right_motor = right_motor / 557
 
-    X_new = [[left_sensor, right_sensor, left_motor_count, right_motor_count]]
+    X_new = [[col_left, col_right, left_motor, right_motor]]
 
-    print(str(left_sensor) + " " + str(right_sensor) + " " + str(left_motor_count) + " " + str(right_motor_count))
+    print(str(col_left) + " " + str(col_right) + " " + str(left_motor) + " " + str(right_motor))
 
     # Load model and scaler
     loaded_model = pickle.load(open("trained_model.sav", 'rb'))
@@ -32,4 +69,3 @@ def classify():
     Sound.speak(letters[y_new[0]])
 
 run()
-classify()
